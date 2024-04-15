dbddf92b7dc3844b2d13b5ada6141265f785f7ae
Alice-Az <75574982+Alice-Az@users.noreply.github.com>
2024-04-15 12:46:29 +0200

    test

--- src/components/WeatherContainers/WeatherDetails/DailyTab/DailyTab.jsx
+++ src/components/WeatherContainers/WeatherDetails/DailyTab/DailyTab.jsx
@@ -1,23 +1,31 @@
-import './DailyTab.css';
-import WeatherIcon from '../../../icons/weatherIcon/weatherIcon';
+import "./DailyTab.css";
+// import WeatherIcon from '../../../icons/weatherIcon/weatherIcon';
 
 const DailyTab = (props) => {
-
     return (
-        <div className='daily-table'>
-                {props.isLoading? <></> : props.forecastDay.map(day =>
-                    <div key={day.Date} className='daily-row'>
-                        <div className='daily-date'>{day.Date}</div>
-                        <div className='daily-data-min'>
-                            <span className='daily-data-text'>Min: </span>
+        <div className="daily-table">
+            {props.isLoading ? (
+                <></>
+            ) : (
+                props.forecastDay.map((day) => (
+                    <div key={day.Date} className="daily-row">
+                        <div className="daily-date">{day.Date}</div>
+                        <div className="daily-data-min">
+                            <span className="daily-data-text">Min: </span>
                             {day.TemperatureMin}
-                            <span className='daily-data-small'>&ensp;to&ensp;</span>
+                            <span className="daily-data-small">
+                                &ensp;to&ensp;
+                            </span>
                         </div>
-                        <div className='daily-data-max'><span className='daily-data-text'>Max: </span>{day.TemperatureMax}</div>
-                        <div className='daily-icon'><WeatherIcon weatherNr={day.WeatherIcon}/></div>
-                        <div className='daily-icon-text'>{day.WeatherText}</div>
+                        <div className="daily-data-max">
+                            <span className="daily-data-text">Max: </span>
+                            {day.TemperatureMax}
+                        </div>
+                        {/* <div className='daily-icon'><WeatherIcon weatherNr={day.WeatherIcon}/></div> */}
+                        <div className="daily-icon-text">{day.WeatherText}</div>
                     </div>
-                )}
+                ))
+            )}
         </div>
     );
 };
--- src/components/WeatherContainers/WeatherDetails/HourlyTab/HourlyTab.jsx
+++ src/components/WeatherContainers/WeatherDetails/HourlyTab/HourlyTab.jsx
@@ -1,18 +1,23 @@
-import './HourlyTab.css';
-import WeatherIcon from '../../../icons/weatherIcon/weatherIcon';
+import "./HourlyTab.css";
+// import WeatherIcon from '../../../icons/weatherIcon/weatherIcon';
 
 const HourlyTab = (props) => {
-
     return (
-        <table className='hourly-table'>
+        <table className="hourly-table">
             <tbody>
-                {props.isLoading? <></> : props.forecastHr.map(hour =>
-                    <tr key={hour.DateTime}>
-                        <td className='hourly-date'>{hour.DateTime}</td>
-                        <td className='hourly-data'>{hour.Temperature}</td>
-                        <td className='hourly-icon'><WeatherIcon weatherNr={hour.WeatherIcon}/></td>
-                        <td className='hourly-icon-text'>{hour.WeatherText}</td>
-                    </tr>
+                {props.isLoading ? (
+                    <></>
+                ) : (
+                    props.forecastHr.map((hour) => (
+                        <tr key={hour.DateTime}>
+                            <td className="hourly-date">{hour.DateTime}</td>
+                            <td className="hourly-data">{hour.Temperature}</td>
+                            {/* <td className='hourly-icon'><WeatherIcon weatherNr={hour.WeatherIcon}/></td> */}
+                            <td className="hourly-icon-text">
+                                {hour.WeatherText}
+                            </td>
+                        </tr>
+                    ))
                 )}
             </tbody>
         </table>
